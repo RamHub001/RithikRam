037fb693160a4ccce308276af8fbcae67e8e188b
Rithikram <rithikram.m@rently.com>
2025-09-27 17:13:26 +0530

    feat: support newline as delimiter and support custom

--- string_calculator/app/services/string_calculator_service.rb
+++ string_calculator/app/services/string_calculator_service.rb
@@ -2,6 +2,12 @@ class StringCalculatorService
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.new(Regexp.escape(header[2..]))
+    end
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
--- string_calculator/spec/services/string_calculator_service_spec.rb
+++ string_calculator/spec/services/string_calculator_service_spec.rb
@@ -19,5 +19,13 @@ RSpec.describe StringCalculatorService do
     it "handles any amount of numbers" do
       expect(calculator.add("1,2,3,4,5")).to eq 15
     end
+
+    it "handles new lines between numbers" do
+      expect(calculator.add("1\n2,3")).to eq 6
+    end
+
+    it "supports custom delimiters" do
+      expect(calculator.add("//;\n1;2")).to eq 3
+    end
   end
 end
